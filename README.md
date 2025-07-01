62047feaa8c4c7de8120c0941a99514131bf4fa1
fubblea <ajay.arthur@gmail.com>
2025-07-01 02:48:53 -0400

    Setup doctest

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fluent Python
+
+These are experiments from the book Fluent Python 2nd Ed.
---
+++ src/fluent_python/chapter_1/collection_api.py
@@ -0,0 +1,14 @@
+def collection_api(a, b):
+    """[TODO:description]
+
+    Args:
+        a ([TODO:parameter]): [TODO:description]
+        b ([TODO:parameter]): [TODO:description]
+
+    Returns:
+        [TODO:return]
+
+    >>> collection_api(1,2)
+    3
+    """
+    return a + b
